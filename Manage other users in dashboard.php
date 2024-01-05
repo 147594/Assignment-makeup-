2dc5235ae725e5bbae797ae6870f94cb01f03588
147594 <151981399+147594@users.noreply.github.com>
2024-01-05 22:07:22 +0300

    Create Manage other users in dashboard.php

---
+++ Manage other users in dashboard.php
@@ -0,0 +1,17 @@
+<?php
+// Inside dashboard.php
+
+if(isset($_POST['addUser'])){
+    // Handle adding a new user logic here
+}
+
+if(isset($_POST['updateUser'])){
+    // Handle updating user details logic here
+}
+
+if(isset($_POST['deleteUser'])){
+    // Handle deleting user logic here
+}
+
+// Your HTML code for managing other users
+?>
